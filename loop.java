259fa61f860ab10be4bb5fceccf20331a25b90a8
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-23 15:05:01 +0530

    Update and rename fact.java to loop.java

---
+++ loop.java
@@ -0,0 +1,11 @@
+
+class loop {
+    public static void main(String[] args) {
+      
+
+        System.out.print("loop");
+      for(int i =1;i>10;i++){
+        System.out.println(i);
+    }
+}
+}
