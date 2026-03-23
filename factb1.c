7305f254ee7f847336780739ac79ff37ec809d0a
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-23 14:39:16 +0530

    Implement factorial function in factb1.c

---
+++ factb1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fact(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int n ;
+    printf("enter the input number:");
+    scanf("%d",&n);
+    printf("Factorial of %d = %d", n, factorial(n));
+    return 0;
+}
